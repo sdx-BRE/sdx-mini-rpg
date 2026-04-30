0a4caaa29ea5e17d0bc044014af58e5b9bb5b479
T Sch <tbs.schndlr95@gmail.com>
2026-04-30 19:56:46 +0200

    [task] - implement strategy interfaces and refactor base resources

---
+++ systems/AbilitySystem/Core/Handlers/DummyTriggerHandler.cs
@@ -0,0 +1,25 @@
+using System;
+using SDX.AbilitySystem.Resources.Trigger;
+
+namespace SDX.AbilitySystem.Core.Handlers
+{
+    public partial class DummyTriggerHandler : IAbilityExecuteTriggerHandler
+    {
+        public event Action Triggered;
+        public event Action<float> TriggeredTick;
+        public event Action Finished;
+
+        private readonly object _blackboard;
+
+        public DummyTriggerHandler(object blackboard)
+        {
+            _blackboard = blackboard;
+        }
+
+        public void Setup(AbilityTrigger data) { }
+        public void Start() { }
+        public void Tick(float delta) { }
+        public void Release() { }
+        public void Cancel() { }
+    }
+}
---
+++ systems/AbilitySystem/Core/Handlers/IAbilityExecuteTriggerHandler.cs
@@ -0,0 +1,18 @@
+using System;
+using SDX.AbilitySystem.Resources.Trigger;
+
+namespace SDX.AbilitySystem.Core.Handlers
+{
+    public interface IAbilityExecuteTriggerHandler
+    {
+        event Action Triggered;
+        event Action<float> TriggeredTick;
+        event Action Finished;
+
+        void Setup(AbilityTrigger data);
+        void Start();
+        void Tick(float delta);
+        void Release();
+        void Cancel();
+    }
+}
