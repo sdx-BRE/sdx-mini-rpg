a2e5ac990a305aa50a14a40f7f14daa2926cf969
T Sch <tbs.schndlr95@gmail.com>
2026-05-01 11:42:58 +0200

    [chore] - finalize infrastructure migration with missing UIDs and cleanup

--- systems/AbilitySystem/Core/Execution/Aiming/IAbilityAimingContext.cs
+++
@@ -1,7 +0,0 @@
-namespace SDX.AbilitySystem.Core.Execution.Aiming
-{
-    public interface IAbilityAimingContext
-    {
-        // Wird später mit Leben gefüllt (z. B. Caster, Position, etc.)
-    }
-}
